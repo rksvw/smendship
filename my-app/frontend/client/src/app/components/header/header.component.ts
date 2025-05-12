d6494387a560e2b43f1593f628e2be53827daba1
rksvw <ritik.vw98@gmail.com>
2025-05-12 19:16:46 +0530

    created the index-page header > nav::work-progress

---
+++ my-app/frontend/client/src/app/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [ CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  isDropdownOpen: Boolean = false;
+  options: string[] = ['Option 1', 'Option 2', 'Option 3'];
+  selectedOption: string | null = null;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen; // * Work like a button 1-click : on , another-click : off
+    console.log("work2")
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    console.log("work")
+    // this.isDropdownOpen = false;
+  }
+}
