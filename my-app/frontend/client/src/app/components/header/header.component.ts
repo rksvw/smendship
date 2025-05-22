34601a60c7b10cd9080d099c9f6f6323204e37bd
rksvw <ritik.vw98@gmail.com>
2025-05-22 16:53:46 +0530

    Revert "added some changes on header components"
    This reverts commit 883ada5c664037060d00b9e3215dd44e8b901086.

--- my-app/frontend/client/src/app/components/header/header.component.ts
+++ my-app/frontend/client/src/app/components/header/header.component.ts
@@ -9,4 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isDropdownOpen: Boolean = false;
+  options: string[] = ['Option 1', 'Option 2', 'Option 3'];
+  selectedOption: string | null = null;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen; // * Work like a button 1-click : on , another-click : off
+  }
+
+  onSelect(option: string) {
+    this.isDropdownOpen = false;
+  }
+
+
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    // this.isDropdownOpen = false;
+  }
 }
